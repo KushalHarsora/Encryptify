1677170e026bb13a32c72a1e7c89494c93b8a880
Kushal Harsora <kushalharsora13@gmail.com>
2024-08-04 18:21:54 +0530

    Process Management Done

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -8,3 +8,23 @@
 // Unix-specific includes
 #include <sys/wait.h>
 #endif
+
+
+ProcessManagement::ProcessManagement() {}
+
+bool ProcessManagement::submitToQueue(std::unique_ptr<Task> task) {
+    taskQueue.push(std::move(task));
+    return true;
+}
+
+void ProcessManagement::executeTasks() {
+    while(!taskQueue.empty()) {
+        // brings the first element from queue for execution
+        std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
+        // Once ownership is transfered then pop from queue
+        taskQueue.pop();
+        std::cout << "Executing task: " << taskToExecute->toString() << std::endl;
+        // takes serialized string and then de-serializes it
+        executeCryption(taskToExecute->toString());
+    }
+}
